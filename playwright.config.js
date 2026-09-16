104ae5a532ff1731e2735f8735bb63e75e36fd55
sribalajikrish <sribalajidit@gmail.com>
2026-09-17 00:38:30 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     // headless: false,
-    headless: !process.env.CI,
+    headless: !process.env.CI ? false,
   },
 
   projects: [
